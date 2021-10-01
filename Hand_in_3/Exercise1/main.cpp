69447242613decc3e8c08e80ac4261ab4ae8d0c9
Kevin S <kevin27@outlook.dk>
2021-10-01 10:10:48 +0200

    01_10

---
+++ Hand_in_3/Exercise1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void findPair(vector<int> v, int target)
+{
+    vector<int>::iterator itr;
+    for (auto i = v.begin(); i < v.end(); i++)
+    {
+        itr = find(v.begin(), v.end(), target - *i);
+        if (itr != i && itr != v.end())
+        {
+            cout << "found pair: " << *i << " " << *itr << endl;
+            return;
+        }
+    }
+    cout << "no pair found" << endl;
+}
+
+int main()
+{
+    vector<int> v;
+    vector<int>::iterator itr;
+
+    v.push_back(8);
+    v.push_back(7);
+    v.push_back(2);
+    v.push_back(5);
+    v.push_back(3);
+    v.push_back(1);
+
+    int target = 10;
+
+    int n = v.size();
+
+    findPair(v, target);
+
+    return 0;
+}
