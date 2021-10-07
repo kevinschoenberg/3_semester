6fc5301078a0d14c8563c93ed35682cd692e6bc9
Kevin S <kevin27@outlook.dk>
2021-10-07 10:23:56 +0200

    07_10

--- Hand_in_3/Exercise2/main.cpp
+++ Hand_in_3/Exercise2/main.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 
 using namespace std;
 
 double f1(double x)
 {
-	return 5*x-22;
+	return 5 * x - 22;
 }
+
 double f2(double x)
 {
-	return 3*exp(x);
+	return 3 * exp(x);
 }
 
 double search(double a, double b, double c, double (*f)(double x))
 {
 	int att = 0;
 
-	while(1)
+	while (1)
 	{
 		att++;
-		double x = (a + b) / 2;
-		// If error is less than e^-10, then we say we found x
-		// This is because there could be an error forced by the precision of the double datatype
-		double errorMargin = 1e-10;
+		double x = ((b - a) / 2) + a;
+
+		double margin = 1e-10;
 		double error = abs(abs(f(x)) - abs(c));
-		if(error < errorMargin)
+		if (error < margin)
 		{
-			cout << "Found x: " << x << " in " << att << " attempts" << ", f(x) = " << f(x) << ", with error: " << abs(abs(f(x)) - abs(c)) << endl;
+			
+			cout << fixed << setprecision(6) << "x: " << x << " att: " << att << endl;
+			cout << fixed << setprecision(3) << "f(x): " << f(x) << endl;
 			return x;
-		} else if (f(x) > c)
+		}
+		else if (f(x) > c)
 		{
 			b = x;
-		} else
+		}
+		else
 		{
 			a = x;
 		}
 	}
 }
+
 int main()
 {
 	cout << "f1" << endl;
 	double x1 = search(0, 10000, 70.3, &f1);
 	cout << "f2" << endl;
-	double x2 = search(0, 10000, 70.3, &f2);
+	double x2 = search(0, 1000, 69.420, &f2);
 
 	return 0;
 }
