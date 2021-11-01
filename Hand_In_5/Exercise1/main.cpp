8b59d17067fd5b8af781b5e4a3ef02349ae392c9
Kevin S <kevin27@outlook.dk>
2021-11-01 21:12:29 +0100

    01-11

--- Hand_In_5/Exercise1/main.cpp
+++ Hand_In_5/Exercise1/main.cpp
@@ -8,18 +8,19 @@ void show(vector<T> v)
 {
 	for (T i:v)
 		cout << v[i] << " ";
+	cout << endl;
 }
 
 int main()
 {
     vector<int> a = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-	cout << "The array before sorting is : \n";
+	cout << "The array before sorting is : " << endl;
 	show(a);
     cout << endl;
 
 	selectSort(a);
 
-	cout << "\n\nThe array after sorting is :\n";
+	cout << "The array after sorting is :" << endl;
 	show(a);
 	return 0;
 }
--- Hand_In_5/Exercise1/sort.h
+++ Hand_In_5/Exercise1/sort.h
@@ -14,22 +14,23 @@ using namespace std;
 template<typename T>
 void selectSort(vector<T> &a)
 {
-    int min;
-    T temp;
-    for (int i = 0; i < a.size(); i++)
+    int min; // O(1)
+    T temp; // O(1)
+    for (int i = 0; i < a.size() - 1; i++) // O(N)
     {
-        min = i;
-        for (int j = i + 1; j < a.size(); j++)
+        min = i; // O(1)
+        for (int j = i + 1; j < a.size(); j++) // (N-1)/2 = O(N)
         {
-            if(a[j] < a[min])
+            if(a[j] < a[min]) // O(1)
             {
-                min = j;
+                min = j; // O(1)
             }
         }
-        temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+        temp = a[i]; // O(1)
+        a[i] = a[min]; // O(1)
+        a[min] = temp; // O(1)
     }
 }
-
 #endif
+
+//Combined we get O(N^2)
