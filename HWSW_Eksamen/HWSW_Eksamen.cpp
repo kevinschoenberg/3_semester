8b80d97c34b408a043cd55fb21f91c7898c2c36c
Martin Michaelsen <martinmichaelsen2901@gmail.com>
2021-12-27 13:29:51 +0100

    27-12

--- HWSW_Eksamen/HWSW_Eksamen.cpp
+++ HWSW_Eksamen/HWSW_Eksamen.cpp
@@ -10,19 +10,32 @@ int sc_main(int sc_argc, char* sc_argv[])
 	m1 mod2("sensor2");
 	m2 mod3("control");
 	m3 mod4("camera");
+	m4 mod5("keyboard");
 	sc_signal<int> data_s1, data_s2;
-	sc_signal<std::vector<int>> data_c;
+	sc_signal<int> data_c;
+	sc_signal<bool> data_vld1, data_vld2, data_vld3, data_vld4;
 
 
 	mod1.port1(data_s1);
 	mod2.port1(data_s2);
-
 	mod3.port1(data_s1);
 	mod3.port2(data_s2);
-	mod3.port3(data_c);
 
+	mod3.port3(data_c);
 	mod4.port3(data_c);
+
 	
+	mod3.vld1(data_vld1);
+	mod3.vld2(data_vld2);
+	mod3.vld3(data_vld3);
+
+	mod1.rdy(data_vld1);
+	mod2.rdy(data_vld2);
+	mod4.rdy(data_vld3);
+
+	mod5.vld1(data_vld4);
+	mod3.rdy(data_vld4);
+
 	sc_start(600, SC_SEC);
 	//clock_t end = clock();
 
--- HWSW_Eksamen/Source.h
+++ HWSW_Eksamen/Source.h
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
+
 using namespace sc_core;
 
 SC_MODULE(m1) {
 	sc_out<int> port1;
+	sc_in<bool> rdy;
 	SC_CTOR(m1) {
 		SC_THREAD(sensor);
-		//dont_initialize();
-		sensitive << port1;
+		sensitive << rdy;
 	}
 
 	void sensor()
@@ -20,18 +21,115 @@ SC_MODULE(m1) {
 
 		while (true)
 		{
-			val = rand() % 2;
-			wait(10, SC_MS);
-			port1.write(val);
-			wait(40, SC_MS);
+			if (rdy.read() == true)
+			{
+				val = rand() % 2;
+				port1.write(val);
+				wait(10, SC_MS);
+			}
+			else
+			{
+				wait();
+			}
 		}
 	}
 
 };
 
+SC_MODULE(m3) {
+	sc_out<int> port3;
+	sc_in<bool> rdy;
+	SC_CTOR(m3) {
+		SC_THREAD(camera);
+		sensitive << rdy;
+	}
+
+	void camera()
+	{
+		srand(time(NULL));
+		while (true)
+		{
+			if (rdy.read() == true)
+			{
+				//sum of matrix
+				int sum = 0;
+				//Array containing 4x4 matrix
+				int val[16];
+
+				//mock values for the camera
+				for (int i = 0; i < 16; i++)
+				{
+					val[i] = rand() % 10;
+				}
+
+				//Calculate sum of matrix
+				if (true)
+				{
+					for (int i = 0; i < 16; i++)
+					{
+						sum += val[i];
+					}
+				}
+				//write sum to port
+				
+				wait(20, SC_MS);
+				port3.write(sum);
+			}
+			else
+			{
+				wait();
+			}
+			
+			
+
+			/*
+			
+			for (int i = 0; i < 16; i++)
+				{
+					port3.write(val[i]);
+					vld.write(true);
+					wait(1, SC_MS);
+					vld.write(false);
+				}
+
+			*/
+
+			//wait(10, SC_SEC);
+		}
+	}
+
+};
+
+SC_MODULE(m4) {
+	sc_out<bool> vld1;
+
+
+	SC_CTOR(m4) {
+		SC_THREAD(pin);
+		//dont_initialize();
+	}
+
+	void pin()
+	{
+		vld1.write(false);
+
+			int pin;
+			std::cin >> pin;
+			if (pin ==1234)
+			{
+				vld1.write(true);
+			}
+			else
+			{
+				vld1.write(false);
+			}
+	}
+};
+
 SC_MODULE(m2) {
-	sc_in<int> port1, port2;
-	sc_in<std::vector<int>> port3;
+	sc_in<int> port1, port2, port3;
+	sc_out<bool> vld1, vld2, vld3;
+	sc_in<bool> rdy;
 
 	SC_CTOR(m2) {
 		SC_THREAD(dataanalysis);
@@ -39,70 +137,69 @@ SC_MODULE(m2) {
 		sensitive << port1;
 		sensitive << port2;
 		sensitive << port3;
+		sensitive << rdy;
 	}
-
-	void dataanalysis()
+void dataanalysis()
+{
+	int sen1, sen2, cam1, in_wait;
+	int itr = 0;
+	while (true)
 	{
-		int val1, val2;
-		std::vector<int> val3;
-		while (true)
+		if (rdy.read() == true)
 		{
-			val1 = port1.read();
-			val2 = port2.read();
+			vld1.write(false);
+			vld2.write(false);
+			vld3.write(false);
 
-			val3 = port3.read();
-			
-			
-			
-
-			wait(10, SC_SEC);
-			for (int i = 0; i < 16; i++)
+			//read values from sensors and cameras
+			if (itr % 2 == 0)
 			{
-				std::cout << val3[i];
+				vld3.write(true);
+				vld1.write(true);
+				vld2.write(true);
+				wait(21, SC_MS);
+				vld3.write(false);
+				vld1.write(false);
+				vld2.write(false);
+				cam1 = port3.read();
+				sen1 = port1.read();
+				sen2 = port2.read();
+				wait(29, SC_MS);
 			}
-			std::cout << std::endl;
-			
-			// intruder function
-			
-			wait(50, SC_MS);
+			else
+			{
+				vld1.write(true);
+				vld2.write(true);
+				wait(11, SC_MS);
+				vld1.write(false);
+				vld2.write(false);
+				sen1 = port1.read();
+				sen2 = port2.read();
+				wait(39, SC_MS);
+			}
+			itr++;
+
+			std::cout << cam1 << std::endl;
+			std::cout << cam1 * (sen1 + sen2) << std::endl;
+
+			//Check for intrution
+			int i_m = cam1 * (sen1 + sen2);
+			if (i_m > 1)
+			{
+				std::cout << "intrution detected!" << std::endl;
+				wait(10, SC_SEC);
+				std::cout << "10 sec passed" << std::endl;
+			}
+			else
+			{
+				std::cout << "all is well" << std::endl;
+			}
 		}
-	}
-
-};
-
-
-SC_MODULE(m3) {
-	sc_out<std::vector<int>> port3;
-	SC_CTOR(m3) {
-		SC_THREAD(camera);
-		/*
-		for (int i = 0; i < 16; i++)
+		else
 		{
-			sensitive << port3;
-		}
-		*/
-		//sensitive << port3;
-	}
-
-	void camera()
-	{
-		
-		srand(time(NULL));
-
-		while (true)
-		{
-			std::vector<int> val;
-			for (int i = 0; i < 16; i++)
-			{
-				val.push_back(i);
-			}
-			
-			for (int i = 0; i < 16; i++)
-			{
-				port3.write(val);
-			}
 			wait();
 		}
 	}
+}
 
 };
